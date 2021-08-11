4fc9ff65c988884632b81f4a99ec55f3c489c47c
guillermina <manzanog0612@gmail.com>
2021-08-11 10:57:38 -0300

    Added project with functional menu.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace TSDV_Final.Management
+{
+    public class GameManager : MonoBehaviour
+    {
+        private static GameManager instance = null;
+        public static GameManager Instance { get => instance; }
+
+        private void Awake()
+        {
+            if (instance != null)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+        }
+
+        public enum Scene { Menu, Game, ResultScreen }
+
+        public void GoToScene(Scene scene)
+        {
+            string stringSceneName;
+
+            switch (scene)
+            {
+                case Scene.Menu:
+                    stringSceneName = "Menu";
+                    break;
+                case Scene.Game:
+                    stringSceneName = "Game";
+                    break;
+                case Scene.ResultScreen:
+                    stringSceneName = "ResultScreen";
+                    break;
+                default:
+                    stringSceneName = "Menu";
+                    break;
+            }
+
+            SceneManager.LoadScene(stringSceneName);
+
+            if (scene == Scene.ResultScreen)
+            {
+                //*SetPlayerData();
+            }
+        }
+
+        public void CloseGame()
+        {
+            Application.Quit();
+        }
+    }
+}
---
+++ Assets/Scripts/MainMenu/UI/UIMenuMananger.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using TSDV_Final.Management;
+
+namespace TSDV_Final.Menu.UI
+{
+    public class UIMenuMananger : MonoBehaviour
+    {
+        [SerializeField] private GameObject menu = null;
+        [SerializeField] private GameObject options = null;
+        [SerializeField] private GameObject credits = null;
+
+        [SerializeField] private GameObject[] creditsPanels = null;
+        [SerializeField] private GameObject[] creditsPanelsButtons = null;
+
+        private enum Menu { Main, Options, Credits}
+        private enum CreditsPanel { Names, Assets}
+        
+        public void GoToScene(int scene)
+        {
+            GameManager.Instance.GoToScene((GameManager.Scene)scene);
+        }
+
+        public void SwitchToCreditsPanel(int panel)
+        {
+            CreditsPanel creditsPanel = (CreditsPanel)panel;
+
+            creditsPanelsButtons[0].SetActive(false);
+            creditsPanelsButtons[1].SetActive(false);
+            creditsPanels[0].SetActive(false);
+            creditsPanels[1].SetActive(false);
+
+            switch (creditsPanel)
+            {
+                case CreditsPanel.Names:
+                    creditsPanelsButtons[1].SetActive(true);
+                    break;
+                case CreditsPanel.Assets:
+                    creditsPanelsButtons[0].SetActive(true);                    
+                    break;
+                default:
+                    break;
+            }
+
+            creditsPanels[panel].SetActive(true);
+        }
+
+        public void SwitchToPanel(int panel)
+        {
+            Menu menuPanel = (Menu)panel;
+
+            menu.SetActive(false);
+            credits.SetActive(false);
+            options.SetActive(false);
+
+            switch (menuPanel)
+            {
+                case Menu.Main:
+                    menu.SetActive(true);
+                    break;
+                case Menu.Options:
+                    options.SetActive(true);
+                    break;
+                case Menu.Credits:
+                    credits.SetActive(true);
+                    SwitchToCreditsPanel(0);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public void ExitGame()
+        {
+            GameManager.Instance.CloseGame();
+        }
+    }
+}
