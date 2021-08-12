0899ddf2d21ca76c6b398c3740c72e2e654ccd6b
guillermina <manzanog0612@gmail.com>
2021-08-11 21:37:11 -0300

    Added platforms generation and pool.

---
+++ Assets/Scripts/Gameplay/Platforms/Platform.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EndlessT4cos.Gameplay.Platforms
+{
+    public class Platform : MonoBehaviour
+    {
+        [SerializeField] private Row row = Row.Middle;
+        private Vector2 halfSize = Vector2.zero;
+        private Action<GameObject> onOutOfScreen = null;
+        private float speed = 5f;
+
+        public Row Row { get => row; set => row = value; }
+        public Vector2 HalfSize { get => halfSize; set => halfSize = value; }
+        public Action<GameObject> OnOutOfScreen { get => onOutOfScreen; set => onOutOfScreen = value; }
+        public float Speed { get => speed; set => speed = value; }
+
+        public void Move()
+        {
+            transform.position -= Vector3.right * speed * Time.deltaTime;
+        }
+
+        private void Start()
+        {
+            halfSize.x = transform.lossyScale.x / 2f;
+            halfSize.y = transform.lossyScale.y / 2f;
+        }
+    }
+}
---
+++ Assets/Scripts/Gameplay/Platforms/PlatformsManager.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EndlessT4cos.Gameplay.Platforms
+{
+    public enum Row { Up, Middle, Down }
+
+    public class PlatformsManager : MonoBehaviour
+    {
+        [SerializeField] private GameObject[] platforms = null;
+        [SerializeField] private Queue<GameObject> platformPool = null;
+
+        [SerializeField] private float halfSizeOfScreen = 8.9f;
+
+        [SerializeField] private float[] yPosition = null;
+        private float distance = 1f;
+
+        private void PlacePlatform(Row row, GameObject platformGO)
+        {
+            Platform platform = platformGO.GetComponent<Platform>();
+
+            platform.transform.position = new Vector2(halfSizeOfScreen + platform.HalfSize.x + distance, yPosition[(int)row]);
+            platform.Row = row;
+        }
+
+        private void DeactivatePlatform(GameObject platform)
+        {
+            platform.SetActive(false);
+            platformPool.Enqueue(platform);
+        }
+
+        private GameObject ActivatePlatform()
+        {
+            GameObject platform = platformPool.Dequeue();
+
+            while (platform.activeSelf)
+            {
+                platformPool.Enqueue(platform);
+                platform = platformPool.Dequeue();
+            }
+            
+            platform.SetActive(true);
+
+            return platform;
+        }
+
+        private void Start()
+        {
+            platformPool = new Queue<GameObject>();
+
+            for (int i = 0; i < platforms.Length; i++)
+            {
+                platformPool.Enqueue(platforms[i]);
+            }
+        }
+
+        private void Update()
+        {
+            Platform platform = null;
+
+            for (int i = 0; i < platforms.Length; i++)
+            {
+                if (platforms[i].activeSelf)
+                {
+                    platform = platforms[i].GetComponent<Platform>();
+                    platform.Move();
+
+                    if (platforms[i].transform.position.x + platform.HalfSize.x < -halfSizeOfScreen)
+                    {
+                        DeactivatePlatform(platforms[i]);
+
+                        GameObject newPlatform = ActivatePlatform();
+                        PlacePlatform(platform.Row, newPlatform);
+                    }
+                }
+            }
+        }
+    }
+}
