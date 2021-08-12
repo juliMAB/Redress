3868e7a990ab1c733ba1d6feaef79a57464b6ce0
juliMAB <julian6aguirre@hotmail.com>
2021-08-12 12:05:46 -0300

    Change GoDown whit floor

---
+++ Assets/Scripts/JuliScene/VerticalPlataform.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerticalPlataform : MonoBehaviour
+{
+    private PlatformEffector2D effector;
+    [SerializeField]float waitTime;
+    private void Start()
+    {
+        effector = GetComponent<PlatformEffector2D>();
+    }
+    private void Update()
+    {
+        if (Input.GetButtonUp("Crouch"))
+        {
+            waitTime = 0.5f;
+        }
+        if (Input.GetButton("Crouch"))
+        {
+            if (waitTime<=0)
+            {
+                effector.rotationalOffset = 180f;
+                waitTime = 0.5f;
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
+        }
+
+        if (Input.GetButton("Jump"))
+        {
+            effector.rotationalOffset = 0;
+        }
+    }
+}
