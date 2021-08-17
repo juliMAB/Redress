e7e344074edb540780227c89424767998786e42d
guillermina <manzanog0612@gmail.com>
2021-08-17 08:27:44 -0300

    Organized code and project.

---
+++ Assets/Scripts/Gameplay/Platforms/VerticalPlataform.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EndlessT4cos.Gameplay.Platforms
+{
+    public class VerticalPlataform : MonoBehaviour
+    {
+        private Collider2D Collider;
+
+        private void Start()
+        {
+            Collider = GetComponent<Collider2D>();
+        }
+
+        private void Update()
+        {
+            if (Input.GetButton("Crouch"))
+            {
+                Collider.isTrigger = true;
+            }
+            else
+            {
+                Collider.isTrigger = false;
+            }
+        }
+    }
+}
+
+
