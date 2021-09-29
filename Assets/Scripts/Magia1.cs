ead32ea63c2d48f6844a7a987205538235bf082e
juliMAB <julian6aguirre@hotmail.com>
2021-09-29 16:42:05 -0300

    Fix lose references.

---
+++ Assets/Scripts/Magia1.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class Magia1 : MonoBehaviour
+{
+    [SerializeField] Button ejemplo;
+
+    Button[] todos;
+
+    private void OnEnable()
+    {
+        todos = FindObjectsOfType<Button>();
+
+        foreach (var item in todos)
+        {
+            item.colors = ejemplo.colors;
+            item.GetComponent<Image>().color = ejemplo.GetComponent<Image>().color;
+        }
+        
+    }
+}
