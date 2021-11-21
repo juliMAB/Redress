4e6b3f62187e581e4fa798c34bc13955e7983583
juliMAB <julian6aguirre@hotmail.com>
2021-11-21 19:17:34 -0300

    Slider modifiquer working.

---
+++ Assets/Scripts/Gameplay/Wise/AudioController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AK;
+using UnityEngine.UI;
+
+public class AudioController : MonoBehaviour
+{
+    [SerializeField] Slider slider;
+   public void OnCall()
+    {
+        AkSoundEngine.SetRTPCValue("VolumenFx", slider.value * 10);
+        AkSoundEngine.PostEvent("play_laser", gameObject);
+    }
+}
