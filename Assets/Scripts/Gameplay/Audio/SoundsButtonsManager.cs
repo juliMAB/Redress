a7754d684c7222e357b5d1a5b91251fa15c20089
juliMAB <julian6aguirre@hotmail.com>
2021-12-10 20:33:44 -0300

    Sort Folders, names and Scripts (WIP)

---
+++ Assets/Scripts/Gameplay/Audio/SoundsButtonsManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Redress.Audio
+{
+
+
+    public class SoundsButtonsManager : MonoBehaviour
+    {
+        [SerializeField] AK.Wwise.Event onHover;
+        [SerializeField] AK.Wwise.Event onclick;
+        [SerializeField] AK.Wwise.Event onPlay;
+
+        public void OnHoverSound()
+        {
+            onHover.Post(gameObject);
+        }
+        public void OnClickSound()
+        {
+            onclick.Post(gameObject);
+        }
+        public void OnPlay()
+        {
+            onPlay.Post(gameObject);
+        }
+    }
+}
+
